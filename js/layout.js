29ed4bae518493ba421412b8840ac926e838ebea
김윤서 <ozil1128@naver.com>
2024-08-23 16:18:50 +0900

    [docs] 모바일 반응형- 메인페이지

---
+++ js/layout.js
@@ -0,0 +1,89 @@
+"use strict";
+
+$(document).ready(function () {
+  //미트립 랭킹
+  $(".con2_sec").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+
+  //미트립 서비스
+  $(".con1_wrapper").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    dots: false,
+    prevArrow: $("#arrow_left"),
+    nextArrow: $("#arrow_right"),
+  });
+
+  const slider = $(".con2_sec");
+  let slickOptions = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    dots: false,
+    prevArrow: $("#arrow_left"),
+    nextArrow: $("#arrow_right"),
+  };
+
+  $(window).on("load resize", function () {
+    if ($(window).width() < 769) {
+      slider.slick("unslick");
+    } else {
+      slider.not(".con1_wrapper").slick(slickOptions);
+    }
+  });
+
+  //미트립 리뷰
+  $(".con5_sec").slick({
+    rows: 1,
+    mobileFirst: true,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: false,
+    variableWidth: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: "unslick",
+      },
+    ],
+  });
+  $(window).on("resize", function () {
+    $(".con5_sec").slick("resize");
+  });
+});
